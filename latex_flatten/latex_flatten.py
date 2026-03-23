fef2b0bfe39f3511e5acfb71d82bf012eb0b66e1
pponsv <ponsvpedro@gmail.com>
2026-03-23 14:57:03 +0100

    Refactored code

--- latex_flatten/latex_flatten.py
+++ latex_flatten/latex_flatten.py
@@ -2,48 +2,64 @@
 
 # A simple script for flattening LaTeX files by inlining included files.
 #
-#   - Supports `\include` and `\input` commands.
-#   - Automatically adds extension `.tex` if the file does not have an extension.
-#   - Handles multiple include commands per line, comments.
-#   - Does not flatten recursively.
+# - Supports `\include` and `\input` commands.
+# - Automatically adds extension `.tex` if the file does not have an extension.
+# - Handles multiple include commands per line, comments.
+# - Does not flatten recursively.
 
 import re
-import sys
 
-if len(sys.argv)==3:
+
+def flatten_latex(main_name, output_name):
+    """
+    Flattens a LaTeX file by inlining included files.
+
+    Args:
+        main_name (str): Path to the main LaTeX file.
+        output_name (str): Path to the output flattened file.
+    """
+    with open(main_name, "r") as main, open(output_name, "w") as output:
+        for line in main.readlines():
+            s = re.split("%", line, 2)
+            tex = s[0]
+            if len(s) > 1:
+                comment = "%" + s[1]
+            else:
+                comment = ""
+
+            chunks = re.split(r"\\(?:input|include)\{[^}]+\}", tex)
+
+            if len(chunks) > 1:
+                for c, t in zip(
+                    chunks, re.finditer(r"\\(input|include)\{([^}]+)\}", tex)
+                ):
+                    cmd_name = t.group(1)
+                    include_name = t.group(2)
+                    if "." not in include_name:
+                        include_name = include_name + ".tex"
+                    if c.strip():
+                        output.write(c + "\n")
+                    output.write(f"% BEGIN \\{cmd_name}{{{include_name}}}\n")
+                    with open(include_name, "r") as include:
+                        output.write(include.read())
+                    output.write(f"% END \\{cmd_name}{{{include_name}}}\n")
+                tail = chunks[-1] + comment
+                if tail.strip():
+                    output.write(tail)
+            else:
+                output.write(line)
+
+
+def main():
+    import sys
+
+    if len(sys.argv) != 3:
+        sys.exit("USAGE: python3 -m latex_flatten.latex_flatten main.tex output.tex")
+
     main_name = sys.argv[1]
     output_name = sys.argv[2]
-else:
-    sys.exit('USAGE: %s main.tex output.tex' %sys.argv[0])
+    flatten_latex(main_name, output_name)
 
-main = open(main_name,'r')
-output = open(output_name,'w')
 
-for line in main.readlines():
-    s = re.split('%', line, 2)
-    tex = s[0]
-    if len(s) > 1:
-        comment = '%' + s[1]
-    else:
-        comment = ''
-
-    chunks = re.split(r'\\(?:input|include)\{[^}]+\}', tex)
-
-    if len(chunks) > 1:
-        for (c, t) in zip(chunks, re.finditer(r'\\(input|include)\{([^}]+)\}', tex)):
-            cmd_name = t.group(1)
-            include_name = t.group(2)
-            if '.' not in include_name: include_name = include_name + '.tex'
-            if c.strip(): output.write(c + '\n')
-            output.write('% BEGIN \\' + cmd_name + '{' + include_name + '}\n')
-            include = open(include_name, 'r')
-            output.write(include.read())
-            include.close()
-            output.write('% END \\' + cmd_name + '{' + include_name + '}\n')
-        tail = chunks[-1] + comment
-        if tail.strip(): output.write(tail)
-    else:
-        output.write(line)
-
-output.close()
-main.close()
+if __name__ == "__main__":
+    main()
